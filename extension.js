00964b035e1b181c4aa0b3c05f35f6166d2a2e09
小糊涂 <>
2020-07-14 21:24:54 +0800

    新增 插件禁用功能

--- extension.js
+++ extension.js
@@ -6,16 +6,29 @@ var check = require('./src/checkUpdate.js');
 
 //该方法将在插件激活的时候调用
 function activate(context) {
-
-    // form plugins/xxx/package.json, get third plugin command
-    async function getThirdPluginsCommand() {
-        await base.getPluginsCommands();
-    };
-    setTimeout(function() {
-        getThirdPluginsCommand();
-    }, 15);
-
     let commandPalette = hx.commands.registerCommand('extension.commandPalette', (parm) => {
+        // 禁用
+        var config = hx.workspace.getConfiguration();
+        let isCheckUpdate = config.get('commandPalette.isDisable');
+        if (isCheckUpdate) {
+            return hx.window.showErrorMessage('此插件已禁用, 若想使用, 请重新开启。',['启用','我再想想']).then(result => {
+                if (result == '启用') {
+                    config.update("commandPalette.isDisable", false).then(() => {
+                        hx.commands.executeCommand('extension.commandPalette');
+                    });
+                };
+            });
+        };
+
+        // form plugins/xxx/package.json, get third plugin command
+        async function getThirdPluginsCommand() {
+            await base.getPluginsCommands();
+        };
+        setTimeout(function() {
+            getThirdPluginsCommand();
+        }, 15);
+
+        // start
         main.main(parm);
         check.checkUpdate();
     });
