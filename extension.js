b0753f5559bd8a5f3fbf92cdcf1e77e1a55c3ee6
小糊涂 <>
2020-07-14 23:06:09 +0800

    优化 插件禁用

--- extension.js
+++ extension.js
@@ -1,42 +1,36 @@
 const hx = require('hbuilderx');
-
+const path = require('path');
 const main = require('./src/main.js');
 const base = require('./src/utils/base.js');
 var check = require('./src/checkUpdate.js');
 
-function showMsg(config) {
-    return hx.window.showErrorMessage('此插件已禁用, 若想使用, 请重新开启。', ['启用', '我再想想']).then(result => {
-        if (result == '启用') {
-            config.update("commandPalette.isDisable", false).then(() => {
-                hx.commands.executeCommand('extension.commandPalette');
-            });
-        };
-    });
-};
+var isScanPlugin = false;
 
 //该方法将在插件激活的时候调用
 function activate(context) {
 
-    // 禁用
     var config = hx.workspace.getConfiguration();
-    let status = config.get('commandPalette.isDisable');
-    if (status) {
-        return showMsg(config);
-    };
 
     // form plugins/xxx/package.json, get third plugin command
     async function getThirdPluginsCommand() {
-        await base.getPluginsCommands();
+        if (!isScanPlugin) {
+            await base.getPluginsCommands();
+            isScanPlugin = true;
+        }
     };
-    setTimeout(function() {
-        getThirdPluginsCommand();
-    }, 15);
 
     let commandPalette = hx.commands.registerCommand('extension.commandPalette', (parm) => {
-        let status1 = config.get('commandPalette.isDisable');
-        if (status1) {
-            return showMsg(config);
+        let status = config.get('commandPalette.isDisable');
+        if (status) {
+            return hx.window.showErrorMessage('此插件已禁用, 若想使用, 请重新开启。', ['启用', '我再想想']).then(result => {
+                if (result == '启用') {
+                    config.update("commandPalette.isDisable", false).then(() => {
+                        hx.commands.executeCommand('extension.commandPalette');
+                    });
+                };
+            });
         } else {
+            getThirdPluginsCommand();
             main.main(parm);
             check.checkUpdate();
         };
