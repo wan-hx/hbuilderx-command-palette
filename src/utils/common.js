2f36a07f11972e6511aad6314c5aa7d0db407d8e
mac mini <shanxi.1991@gmail.com>
2020-07-15 17:26:39 +0800

    fixed

--- src/utils/common.js
+++ src/utils/common.js
@@ -35,11 +35,6 @@ function getPath(parm) {
  * @description 创建输出控制台
  */
 function createOutputChannel(channel_name, info) {
-    let tmp = info.filter( v => v);
-    if (tmp.length == 0) {
-        return;
-    };
-
     // 创建控制台
     let outputChannel = hx.window.createOutputChannel(channel_name);
     // 显示控制台
